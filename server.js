bbcf815bfe80dc7e3e4c84177e0ddea3ad31b6ca
Joseph Brimeyer <54119655+josephbrimeyer@users.noreply.github.com>
2020-06-03 20:09:06 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,69 @@
+let express = require("express");
+let path = require("path");
+let app = express();
+let PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+let tableData = [
+    {
+      name: "",
+      phoneNumber: "",
+      email: "",
+      uniqueID: "",
+    },
+];
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "view.html"));
+//   });
+  
+//   app.get("/reserve", function(req, res) {
+//     res.sendFile(path.join(__dirname, "add.html"));
+//   });
+  
+//   // Displays all characters
+//   app.get("/api/tables", function(req, res) {
+//     return res.json(characters);
+//   });
+  
+//   // Displays a single character, or returns false
+// //   app.get("/api/tables/:table", function(req, res) {
+// //     var chosen = req.params.character;
+  
+//     console.log(chosen);
+  
+//     for (var i = 0; i < characters.length; i++) {
+//       if (chosen === characters[i].routeName) {
+//         return res.json(characters[i]);
+//       }
+//     }
+  
+//     return res.json(false);
+  
+  
+//   // Create New Characters - takes in JSON input
+//   app.post("/api/characters", function(req, res) {
+//     // req.body hosts is equal to the JSON post sent from the user
+//     // This works because of our body parsing middleware
+//     var newCharacter = req.body;
+  
+//     // Using a RegEx Pattern to remove spaces from newCharacter
+//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//     newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+//     console.log(newCharacter);
+  
+//     characters.push(newCharacter);
+  
+//     res.json(newCharacter);
+//   });
+  
+//   // Starts the server to begin listening
+//   // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
